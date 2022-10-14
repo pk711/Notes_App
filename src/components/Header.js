7fedfbdbbaf70aade576ee7b8bdbebc3815c4036
pk711 <114118300+pk711@users.noreply.github.com>
2022-10-14 04:42:14 +0100

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = ({ handleToggleDarkMode }) => {
+  return (
+    <div className="header">
+      <h1>Notes App</h1>
+      <button
+        onClick={() =>
+          handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
+        }
+        className="save"
+      >
+        Dark mode
+      </button>
+    </div>
+  );
+};
+
+export default Header;
